052f147be35cd0ba12fc44044f2a413ab0510e8d
Param <parammoradiya98@gmail.com>
2019-02-13 17:19:34 +0530

    done thodu baki

--- app/src/main/java/com/example/param/emall/ScannerActivity.java
+++ app/src/main/java/com/example/param/emall/ScannerActivity.java
@@ -3,6 +3,7 @@ package com.example.param.emall;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
@@ -17,11 +18,15 @@ import static com.example.param.emall.HomeActivity.m_ref;
 
 public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
     ZXingScannerView scannerView;
+    DatabaseHelper dh;
+    ItemData itemData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         scannerView = new ZXingScannerView(this);
         setContentView(scannerView);
+        dh = new DatabaseHelper(ScannerActivity.this);
+        dh.getWritableDatabase();
     }
 
     @Override
@@ -37,12 +42,20 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
                     //String  code = ds.child("P_code").getValue().toString();
                     String name = ds.child("P_name").getValue().toString();
                     String price = ds.child("p_price").getValue().toString();
-
-
+                    Log.v("name",""+name);
+                   // dh.getWritableDatabase();
+                   //itemData  = new ItemData("name","price","1");
+                   //long i = dh.insertData(name,price,"1");
+                    //Log.v("@@@", ""+i);
+                    //Toast.makeText(ScannerActivity.this,"Saved!!!",Toast.LENGTH_LONG).show();
+                    HomeActivity.name = name;
+                    HomeActivity.price = price;
                     //HomeActivity.m_product_list.add("Product Code :"+code);
+                    for(int i=0;i<1;i++){
                     HomeActivity.m_product_list.add("Product Name: " + name);
                     HomeActivity.m_product_list.add("Product Price: " + price);
-                    HomeActivity.arrayAdapter.notifyDataSetChanged();
+                    HomeActivity.arrayAdapter.notifyDataSetChanged();}
+
                 }
             }
 
