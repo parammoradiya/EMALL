04ea5d70a2b852d9505a8b678b7fef907e33501e
Param <parammoradiya98@gmail.com>
2019-03-09 18:49:16 +0530

    9/3/19

--- app/src/main/java/com/example/param/emall/CartActivity.java
+++ app/src/main/java/com/example/param/emall/CartActivity.java
@@ -7,72 +7,83 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class CartActivity extends AppCompatActivity {
-   ListView listView ;
-   RecyclerView recyclerView;
-   ArrayList<ItemData> list;
-   MyAdapter adapter;
-   DatabaseReference refDb;
 
-
-   private Toolbar OToolbar;
+    private Toolbar OToolbar;
+    private ListView mlistview;
+    TextView pname,pprice,pqua;
+    FirebaseListAdapter adapter;
+    private FirebaseUser mCurrentuser;
+    private DatabaseReference mUserdatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
+        mlistview = (ListView) findViewById(R.id.cart_list);
+
         OToolbar = (Toolbar) findViewById(R.id.Cart_toolbar);
         setSupportActionBar(OToolbar);
         getSupportActionBar().setTitle("CART");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        /*recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        list = new ArrayList<ItemData>();
-        FirebaseUser Current_user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = Current_user.getUid();
-        refDb = FirebaseDatabase.getInstance().getReference().child("user").child(uid).child("product_list");*/
-       // listView = (ListView)findViewById(R.id.list_Data);
+        mCurrentuser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_uid = mCurrentuser.getUid();
+        mUserdatabase = FirebaseDatabase.getInstance().getReference().child("user").child(current_uid);
 
-        /*Bundle recdData = getIntent().getExtras();
-        String myVal = recdData.getString("value");
-        TextView textView = (TextView)findViewById(R.id.textview);
-        textView.setText(myVal);*/
-        //DatabaseHelper db = new DatabaseHelper(CartActivity.this);
-       /* ItemData itemData = new ItemData();
-        itemData = db.DisplayData();
-        list.add(itemData);
-        Log.v("ddd","list"+list.size());
+        Query query = FirebaseDatabase.getInstance().getReference().child("user").child(current_uid).child("product_list");
+        FirebaseListOptions<cartactivitymodel> options = new FirebaseListOptions.Builder<cartactivitymodel>().setLayout(R.layout.cart_layout).setQuery(query, cartactivitymodel.class).build();
 
-        adapter = new MyAdapter(CartActivity.this,list);
-        recyclerView.setAdapter(adapter);*/
+        adapter = new FirebaseListAdapter(options) {
+            @Override
+            protected void populateView(View v, Object model, int position) {
 
-      /* refDb.addValueEventListener(new ValueEventListener() {
-           @Override
-           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.v("hii2", "aaaa");
+                pname = v.findViewById(R.id.cpname);
+                pprice = v.findViewById(R.id.cpprice);
+                pqua = v.findViewById(R.id.cpquantity);
 
-           }
+                cartactivitymodel rep = (cartactivitymodel) model;
 
-           @Override
-           public void onCancelled(@NonNull DatabaseError databaseError) {
+                pname.setText(rep.getName().toString());
+                pprice.setText(rep.getPrice().toString());
+                pqua.setText(rep.getQty().toString());
+            }
+        };
 
-           }
-       });*/
-
-
+        mlistview.setAdapter(adapter);
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        adapter.stopListening();
+    }
 }
+
---
+++ app/src/main/java/com/example/param/emall/cartactivitymodel.java
@@ -0,0 +1,41 @@
+package com.example.param.emall;
+
+public class cartactivitymodel {
+    private String Name;
+    private  String Price;
+    // private  String contact_no;
+    private  String qty;
+
+    public cartactivitymodel() {
+    }
+
+    public cartactivitymodel(String name, String price, String qty) {
+        Name = name;
+        Price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+}
